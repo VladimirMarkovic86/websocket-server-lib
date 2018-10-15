697099675e6045c3e7b0617ebcb115a1a600f29c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:48:49 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject org.vladimir/websocket-server-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/websocket-server-lib "0.1.0"
   :description "WebSocket server library"
-  :url "http://gitlab:1610/VladimirMarkovic86/websocket-server-lib"
+  :url "http://github.com/VladimirMarkovic86/websocket-server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
-                 [org.vladimir/ajax-lib "0.1.0"]]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 ]
+
+  :min-lein-version "2.0.0"
+
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
 
